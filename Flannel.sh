f54faee6c387ab69a1c2ceede17ed08611b9a511
YuTeng Chen <45888688+0yukali0@users.noreply.github.com>
2022-05-10 20:29:36 +0800

    Update Flannel.sh

--- Flannel.sh
+++ Flannel.sh
@@ -1,1 +1,2 @@
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane- node-role.kubernetes.io/master-
